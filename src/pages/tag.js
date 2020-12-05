701845606cd81c32c3f43129c9dbb45d3e7d9cd3
aznic95 <aznic95@gmail.com>
2020-12-05 15:40:26 +0900

    test tag

--- src/pages/tag.js
+++ src/pages/tag.js
@@ -18,14 +18,25 @@ const TagsPage = ({
   const changeFilter = () => {
     //만약 tag.fieldValue 값이 === frontmatter.tag에 있으면
     //frontmatter.tag가 소속한 frontmatter.title 의 문서를 불러온다.
-
-    const textTitle = edges.map(nodes => nodes.node.frontmatter)
-    const taggg = group.map(tag => tag.fieldValue)
-
-    // console.log(taggg)
-    // console.log(textTitle)
+    console.log(taggg)
+    console.log(textTitle)
     // console.log(textTitle[1].tags[0])
+
+    if (result[0] === taggg[0]) {
+      return
+    }
   }
+
+  let count = 0
+  const textTitle = edges.map(nodes => nodes.node.frontmatter)
+  const taggg = group.map(tag => tag.fieldValue)
+  const result = textTitle.reduce((accumulator, item) => {
+    return {
+      ...accumulator,
+      [item.title]: item.tags,
+    }
+  }, {})
+  console.log(result)
 
   return (
     <div>
@@ -36,22 +47,15 @@ const TagsPage = ({
         <ul>
           {group.map(tag => (
             <li key={tag.fieldValue}>
-              <button
-                onChange={changeFilter}
-                onClick={changeFilter}
-                style={{ backgroundColor: "red" }}
-              >
+              <button onClick={result} style={{ backgroundColor: "red" }}>
                 {tag.fieldValue} ({tag.totalCount})
               </button>
             </li>
           ))}
         </ul>
 
-        <ul>
-          {edges.map(nodes => (
-            <li>{nodes.node.frontmatter.title}</li>
-          ))}
-        </ul>
+        <ul>{taggg[1]}</ul>
+        <ul>{textTitle[1].title}</ul>
       </div>
     </div>
   )
