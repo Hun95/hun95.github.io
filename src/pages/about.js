4e4b3fd91d03ae1906f1e9a397f816e4db909986
aznic95 <aznic95@gmail.com>
2020-12-16 22:51:51 +0900

    change code tag in postpage

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,34 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
+import Layout from "../components/layout";
+import styled from "styled-components";
+import SEO from "../components/seo";
+const Box = styled.div`
+  width: 50;
+  height: auto;
+  background-color: red;
 
-import SEO from "../components/seo"
-
+  margin: 10px;
+`;
 const AboutPage = () => (
   <Layout>
     <SEO title="AboutPage" />
-    <h1>About Us</h1>
-  </Layout>
-)
+    <div style={{ display: `inline-flex`, flexFlow: `column-reverse` }}>
+      <Box>hello</Box>
+      <Box>hello</Box>
+      <Box>hello</Box>
+      <Box>hello</Box>
+    </div>
 
-export default AboutPage
+    <div style={{ display: `inline-flex`, flexFlow: `column-reverse` }}>
+      <Box>hello</Box>
+      <Box>hello</Box>
+      <Box>hello</Box>
+      <Box>1</Box>
+    </div>
+  </Layout>
+);
+
+export default AboutPage;
 
 // / want to create localhost:8000/
