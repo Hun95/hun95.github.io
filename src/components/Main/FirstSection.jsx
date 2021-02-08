32d1665f37906427ca67ce04746d46558e3c3ac3
aznic95 <aznic95@gmail.com>
2021-02-08 23:12:08 +0900

    change image

--- src/components/Main/FirstSection.jsx
+++ src/components/Main/FirstSection.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { useGlobalContext } from '../../context/MainPageProvider';
-import imageOne from '../../images/secondSectionImage.jpg';
+import imageOne from '../../images/secondSectionImage.webp';
 const FirstSection = () => {
   const {
     firstRef,
